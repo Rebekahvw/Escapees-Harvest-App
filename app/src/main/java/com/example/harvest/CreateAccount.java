0ee6a0b89ff834d6aca47cb70b5550584f603f3e
Bvw <2344005@students.wits.ac.za>
2022-09-03 03:23:16 +0200

    Latest Harvest

--- app/src/main/java/com/example/harvest/CreateAccount.java
+++ app/src/main/java/com/example/harvest/CreateAccount.java
@@ -3,6 +3,8 @@ package com.example.harvest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.util.Log;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -11,18 +13,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateAccount extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = CreateAccount.class.getSimpleName();
 
     private TextView banner, registerUser;
     private EditText editTextFullName, editTextUsername, editTextEmail, editTextPassword;
     private FirebaseAuth mAuth;
+    private DatabaseReference mRootRef;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 // ...
 // Initialize Firebase Auth
 
@@ -39,7 +51,7 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_create_account);
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
-
+        mRootRef = FirebaseDatabase.getInstance().getReference();
         registerUser = (TextView) findViewById(R.id.createAccount);
         registerUser.setOnClickListener(this);
 
@@ -48,9 +60,6 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
         editTextPassword = (EditText) findViewById(R.id.password);
         editTextUsername = (EditText) findViewById(R.id.username);
 
-//        if(currentUser != null){
-//            reload();
-//        }
     }
 
     @Override
@@ -101,33 +110,89 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
             return;
         }
 
+//        mAuth.createUserWithEmailAndPassword(email,password)
+//                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if(task.isSuccessful()){
+//                            User user = new User(fullName, username, email);
+//
+//                            // Write a message to the database
+//                            //created a user and a document in the users collection
+//                            FirebaseDatabase.getInstance().getReference("Users")
+//                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+//                                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                                @Override
+//                                public void onComplete(@NonNull Task<Void> task) {
+//                                    if (task.isSuccessful()){
+//                                        Toast.makeText(CreateAccount.this, "Account has been created successfully",Toast.LENGTH_LONG).show();
+//                                    }
+//                                    else{
+//                                        Toast.makeText(CreateAccount.this,"Account creation failed",Toast.LENGTH_LONG).show();
+//                                    }
+//                                }
+//                            });
+//                        }else{
+//                            Toast.makeText(CreateAccount.this,"Account creation failed",Toast.LENGTH_LONG).show();
+//                        }
+//                    }
+//                });
 
-        mAuth.createUserWithEmailAndPassword(email,password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            User user = new User(fullName, username, email);
+        mAuth.createUserWithEmailAndPassword(email , password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
 
-                            // Write a message to the database
-                            //created a user and a document in the users collection
-                            FirebaseDatabase.getInstance().getReference("Users")
-                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()){
-                                        Toast.makeText(CreateAccount.this, "Account has been created successfully",Toast.LENGTH_LONG).show();
-                                    }
-                                    else{
-                                        Toast.makeText(CreateAccount.this,"Account creation failed",Toast.LENGTH_LONG).show();
-                                    }
-                                }
-                            });
-                        }else{
-                            Toast.makeText(CreateAccount.this,"Account creation failed",Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
+                HashMap<String , Object> user = new HashMap<>();
+                user.put("fullName" , fullName);
+                user.put("email", email);
+                user.put("username" , username);
+                user.put("id" , mAuth.getCurrentUser().getUid());
+
+                db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                              //  Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                Toast.makeText(CreateAccount.this, "Account created successfully", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(CreateAccount.this , ProfileActivity.class);
+                                startActivity(intent);
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(CreateAccount.this, "Account creation failed", Toast.LENGTH_SHORT).show();
+                                Log.w(TAG, e.toString());
+                            }
+                        });
+
+                //adding it to realtime database
+//                mRootRef.child("Users").child(mAuth.getCurrentUser().getUid()).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        if (task.isSuccessful()){
+//                            Toast.makeText(CreateAccount.this, "Account has been created successfully",Toast.LENGTH_LONG).show();
+//                            Intent intent = new Intent(CreateAccount.this , ProfileActivity.class);
+//                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                            startActivity(intent);
+//                            finish();
+//                        }
+//                    }
+//                });
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(CreateAccount.this,"Account creation failed",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/harvest/CreateLog.java
+++ app/src/main/java/com/example/harvest/CreateLog.java
@@ -3,33 +3,55 @@ package com.example.harvest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.util.Log;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+
 
 
 public class CreateLog extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
     private EditText logName;
-
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private Button addLog;
-
+    private Button returnHome;
+    //from youtube example
+    private static final String TAG = "CreateLog";
+//
+//    private static final String KEY_TITLE = "title";
+//    private static final String KEY_DESCRIPTION = "description";
+    private CollectionReference usersRef = db.collection("users");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_log);
        // mDatabase = FirebaseDatabase.getInstance().getReference("logs");
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+     //   mDatabase = FirebaseDatabase.getInstance().getReference();
         logName = (EditText) findViewById(R.id.logName);
+        returnHome = (Button)findViewById(R.id.returnHome);
+        returnHome.setOnClickListener(view -> {
+            Intent intent = new Intent(CreateLog.this , ProfileActivity.class);
+            startActivity(intent);
+        });
 
         addLog = (Button) findViewById(R.id.addLog);
         addLog.setOnClickListener(view -> {
@@ -38,21 +60,65 @@ public class CreateLog extends AppCompatActivity {
     }
 
     void createLog(){
-        FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
-        String uid = currentFirebaseUser.getUid();
-        Log log = new Log(uid, logName.getText().toString().trim());
 
-        mDatabase.child("logs").setValue(log).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()){
-                    Toast.makeText(CreateLog.this, "Log created",Toast.LENGTH_LONG).show();
-                }
-                else{
-                    Toast.makeText(CreateLog.this,"Log creation failed",Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+
+        if (logName.getText().toString().trim().isEmpty()){
+            logName.setError("Full name is a required field");
+            logName.requestFocus();
+            return;
+        }
+//        FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+//        String uid = currentFirebaseUser.getUid();
+//        Log log = new Log(uid, logName.getText().toString().trim());
+//
+//        mDatabase.child("logs").setValue(log).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                if (task.isSuccessful()){
+//                    Toast.makeText(CreateLog.this, "Log created",Toast.LENGTH_LONG).show();
+//                }
+//                else{
+//                    Toast.makeText(CreateLog.this,"Log creation failed",Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+
+        //define type for the Key (String) and value that we want to pass (object because we can pass different types)
+        HashMap<String , Object> logs = new HashMap<>();//use hashmap which is a specific implementation of the map interface
+
+        logs.put("logName" , logName.getText().toString().trim());
+//        db.collection("userLogs").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).set(logs, SetOptions.merge()).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                if (task.isSuccessful()){
+//                    Toast.makeText(CreateLog.this, "Log has been created successfully",Toast.LENGTH_LONG).show();
+//                    Intent intent = new Intent(CreateLog.this , ProfileActivity.class);
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                    startActivity(intent);
+//                    finish();
+//                }
+//                else{
+//                    Toast.makeText(CreateLog.this, "Log creation failed",Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+       usersRef.document(FirebaseAuth.getInstance().getCurrentUser().getUid()).collection("Logs").add(logs)
+               .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                   @Override
+                   public void onSuccess(DocumentReference documentReference) {
+                       //  Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                       Toast.makeText(CreateLog.this, "Log added successfully", Toast.LENGTH_LONG).show();
+                       logName.getText().clear();
+                   }
+               })
+               .addOnFailureListener(new OnFailureListener() {
+                   @Override
+                   public void onFailure(@NonNull Exception e) {
+                       Toast.makeText(CreateLog.this, "Log creation failed", Toast.LENGTH_LONG).show();
+                       Log.w(TAG, e.toString());
+                   }
+               });
+
     }
 
 }
